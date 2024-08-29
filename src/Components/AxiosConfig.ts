5943ef6a862beeadeda5d6235ca172d52149c908
Ajith-NM <ajith.n.m5603@gmail.com>
2024-08-29 17:48:56 +0530

    change backend url

--- src/Components/AxiosConfig.ts
+++ src/Components/AxiosConfig.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
  export const request=axios.create({
-    baseURL:"https://todo-backend-mp1m.onrender.com/",
+    baseURL:"https://todo-api-mysql.onrender.com/",
     withCredentials:true,
    // headers:{
      // "Access-Control-Allow-Origin": "https://todolistfocus.netlify.app",
