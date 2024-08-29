26c208ea213724aae89440491b7d2dc495c6cddd
Ajith-NM <ajith.n.m5603@gmail.com>
2024-08-29 17:22:44 +0530

    disabled header

--- src/Components/AxiosConfig.ts
+++ src/Components/AxiosConfig.ts
@@ -4,8 +4,8 @@ import axios from "axios";
  export const request=axios.create({
     baseURL:"https://todo-backend-mp1m.onrender.com/",
     withCredentials:true,
-     headers:{
+    // headers:{
       // Accept: 'application/json',
        //'Content-Type': 'application/json',
-     }
+    // }
   })
