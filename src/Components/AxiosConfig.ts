b27b7e7829b8bf17abc21f5fb2ec05f588920884
Ajith-NM <ajith.n.m5603@gmail.com>
2024-08-29 16:28:47 +0530

    changed credentials

--- src/Components/AxiosConfig.ts
+++ src/Components/AxiosConfig.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
  export const request=axios.create({
     baseURL:"https://todo-backend-mp1m.onrender.com/",
-    withCredentials:true,
-    // headers:{
-    //   Accept: 'application/json',
-    //   'Content-Type': 'application/json',
-    // }
+   // withCredentials:true,
+     headers:{
+      // Accept: 'application/json',
+       //'Content-Type': 'application/json',
+     }
   })
