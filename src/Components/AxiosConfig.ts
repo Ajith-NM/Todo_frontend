450ba88ca2e6ee4bc1dfefbe7a6098f0b039fa7d
Ajith-NM <ajith.n.m5603@gmail.com>
2024-08-30 10:04:35 +0530

    Change axios config

--- src/Components/AxiosConfig.ts
+++ src/Components/AxiosConfig.ts
@@ -4,9 +4,9 @@ import axios from "axios";
  export const request=axios.create({
     baseURL:"https://todo-api-mysql.onrender.com/",
     withCredentials:true,
-   // headers:{
-     // "Access-Control-Allow-Origin": "https://todolistfocus.netlify.app",
+    headers:{
+      "Access-Control-Allow-Origin": "https://todolistfocus.netlify.app",
       //Accept: 'application/json',
       // 'Content-Type': 'application/json',
-   // } 
+    } 
   })
