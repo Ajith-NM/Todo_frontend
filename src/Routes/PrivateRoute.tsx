7beeae866c0e537a0d9beb5fc45cbaa32f159bbd
Ajith-NM <ajith.n.m5603@gmail.com>
2024-09-06 16:24:07 +0530

    Remove token checking from private route

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -1,28 +1,11 @@
 
-import { jwtDecode } from "jwt-decode";
-import { useRef } from "react";
+
 import { Outlet, Navigate } from "react-router-dom";
 
 
 const PrivateRoute = () => {
-  const cookies = useRef(
-    document.cookie
-      .split(";")
-      .reduce(
-        (ac, cv) => Object.assign(ac, { [cv.split("=")[0]]: cv.split("=")[1] }),
-        {}
-      )
-  );
-  const resCookie: Record<string, string> = JSON.parse(
-    JSON.stringify(cookies.current)
-  );
-  const token = resCookie[" token"];
-  const decodedToken: { exp: number } = jwtDecode<{ exp: number }>(token)
-  console.log("decoded Token=", decodedToken.exp);
- const currentTime = Date.now() / 1000
-const userLogged=useRef(currentTime < decodedToken.exp)
 
-    if (userLogged&&localStorage.getItem('user')) {
+    if (localStorage.getItem('user')) {
         return <Outlet/> 
     }
   return  <Navigate to={"/"}></Navigate>
