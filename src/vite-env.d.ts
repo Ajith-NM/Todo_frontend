ee107e368758724642bb95b33ebfda51bf8faf37
Ajith-NM <ajith.n.m5603@gmail.com>
2024-09-20 10:20:35 +0530

    Add type to dotenv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_Base_Url: string;
+    readonly VITE_clientId: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
